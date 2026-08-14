1eae960eeed7b6be1cdf5f873e11c91c0c58bab8
VladislavTsytrikov <FlackMC145@gmail.com>
2026-08-14 13:54:45 +0300

    bb: controlled BuildKit cache reader B

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const fs = require('fs');
 const http = require('http');
 
-const body = JSON.stringify({ marker: 'BB_CACHE_WRITER_A_RUNTIME' });
+let body = JSON.stringify({ marker: 'BB_CACHE_READER_B', error: 'proof_missing' });
+try {
+  body = fs.readFileSync('/app/d.json', 'utf8');
+} catch (_) {}
 
 http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'application/json' });
   response.end(body);
-}).listen(3000, () => console.log('cache writer ready'));
+}).listen(3000, () => console.log('cache reader ready'));
